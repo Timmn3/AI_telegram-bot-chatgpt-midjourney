20e7b8ec7f38e13171c8cc6626cf8e8e3d21aece
Tim <timmn@mail.ru>
2025-03-25 09:35:47 +0300

    увеличил лимиты с 2000 до 5000

--- utils/scheduled_tasks/daily_token_reset.py
+++ utils/scheduled_tasks/daily_token_reset.py
@@ -4,8 +4,8 @@ from utils.db import get_conn
 # Пороговые значения токенов
 TOKEN_LIMITS = {
     "tokens_4o_mini": 100_000,
-    "tokens_4o": 2_000,
-    "tokens_o3_mini": 2_000
+    "tokens_4o": 5000,
+    "tokens_o3_mini": 5000
 }
 
 
@@ -14,7 +14,6 @@ async def refill_tokens():
     Проверяет баланс токенов у всех пользователей и пополняет их до лимита, если баланс ниже.
     """
     conn = await get_conn()
-    print("start")
     for token_type, limit in TOKEN_LIMITS.items():
         await conn.execute(
             f"""
@@ -24,7 +23,6 @@ async def refill_tokens():
             """,
             limit
         )
-    print("finish")
     await conn.close()
 
 
